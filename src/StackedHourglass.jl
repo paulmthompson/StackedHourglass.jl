347c262c281c432034470e0cadcbe16a5e5b68e8
Paul Thompson <pmt10@case.edu>
2020-08-03 21:59:22 -0400

    move preprocessing to its own file

--- src/StackedHourglass.jl
+++ src/StackedHourglass.jl
@@ -3,6 +3,8 @@ module StackedHourglass
 #Deep Learning Libraries
 using CuArrays, CuArrays.CUFFT, CUDAnative, Knet
 
+using Images
+
 include("residual")
 include("hourglass.jl")
 
@@ -10,6 +12,7 @@ include("cuda_files.jl")
 include("helper.jl")
 include("load.jl")
 include("subpixel")
+include("image_preprocessing.jl")
 
 
 
