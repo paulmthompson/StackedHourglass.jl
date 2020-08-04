37a2ca4ff8fcdc7ea9c252ede1fd8032ba11d698
Paul Thompson <pmt10@case.edu>
2020-08-03 22:52:34 -0400

    fixed several typos

--- src/StackedHourglass.jl
+++ src/StackedHourglass.jl
@@ -3,15 +3,17 @@ module StackedHourglass
 #Deep Learning Libraries
 using CuArrays, CuArrays.CUFFT, CUDAnative, Knet
 
-using Images
+using Images, Distributed
 
-include("residual")
+abstract type NN end;
+
+include("residual.jl")
 include("hourglass.jl")
 
 include("cuda_files.jl")
 include("helper.jl")
 include("load.jl")
-include("subpixel")
+include("subpixel.jl")
 include("image_preprocessing.jl")
 
 
